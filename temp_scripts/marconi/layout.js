d64b27aa33149af36ebfc2d344bcff420b5735e1
Marco <hictechsrl@gmail.com>
2012-02-24 15:39:58 +0100

    - fixed controller mozilla FF

--- temp_scripts/marconi/layout.js
+++ temp_scripts/marconi/layout.js
@@ -21,20 +21,23 @@ $(document).ready(function(){
 		
 
 		//vertical
-		var expander_h = getViewPort().height 	- parseInt(($(".MAINAREA-header").is(":visible")) ? $(".MAINAREA-header").css("height") : 0)
-												- parseInt(($(".MAINAREA-controller").is(":visible")) ? $(".MAINAREA-controller").css("height") : 0) 
-												- parseInt(($(".MAINAREA-navigator").is(":visible")) ? $(".MAINAREA-navigator").css("height") : 0) 
-												- parseInt(($(".MAIN-header").is(":visible")) ? $(".MAIN-header").css("height") : 0) 
-												- parseInt(($(".MAIN-footer").is(":visible")) ? $(".MAIN-footer").css("height") : 0) 
-												- parseInt(($(".MAINAREA-sheet-footer").is(":visible")) ? $(".MAINAREA-sheet-footer").css("height") : 0);							
-		
+		var expander_h = getViewPort().height 	- parseInt(($(".MAINAREA-header").is(":visible")) ? $(".MAINAREA-header").height() : 0)
+												- parseInt(($(".MAINAREA-navigator").is(":visible")) ? $(".MAINAREA-navigator").height() : 0) 
+												- parseInt(($(".MAIN-header").is(":visible")) ? $(".MAIN-header").height() : 0) 
+												- parseInt(($(".MAIN-footer").is(":visible")) ? $(".MAIN-footer").height() : 0) 
+												- parseInt(($(".MAINAREA-sheet-footer").is(":visible")) ? $(".MAINAREA-sheet-footer").height() : 0);	
+													
+		// inserisco nel conto se e quanti controller ci sono									
+		$(".MAINAREA-controller").each(function(){
+			( $(this).is(":visible") ) ? expander_h= expander_h -  parseInt($(".MAINAREA-controller").height()) : null;			
+		});
 
 		$(".SHEET").css("height",expander_h);
 		
 		
 		if(jQuery.browser.msie && jQuery.browser.version == 7){
-			var tr_expander_h = expander_h + parseInt(($(".MAIN-header").is(":visible")) ? $(".MAIN-header").css("height") : 0) 
-										   + parseInt(($(".MAIN-footer").is(":visible")) ? $(".MAIN-footer").css("height") : 0);
+			var tr_expander_h = expander_h + parseInt(($(".MAIN-header").is(":visible")) ? $(".MAIN-header").height() : 0) 
+										   + parseInt(($(".MAIN-footer").is(":visible")) ? $(".MAIN-footer").height() : 0);
 			$(".SHEET").parents("tr").eq(0).css("height",tr_expander_h);
 		}
 	
@@ -42,32 +45,39 @@ $(document).ready(function(){
 		$(".SHEET").css("width",getViewPort().width - 250);
 		$(".CONTROLLER-wrapper").css("width",getViewPort().width - 235);
 		
-		calculateSlider();	
-		calculateController();
+		calculateSlider();
+		
+		$(".MAINAREA-controller").each(function(){
+			calculateController($(this));
+		})
+		
 	
 	}
 	
 
 	
-	function calculateController(){
+	function calculateController($el){
 		
 		var scroller_tollerance = 70;    // di quanti pixel il contenuto deve eccedere rispetto al contenitore per attivare lo scroller
 		var hidding_arrows_offset = 12;
 		var sensibility = 5;
+		var scrollbar_height = 20;
+		var interval = (jQuery.browser.mozilla) ? 40 : 1; 
 		
 		///////////// calcolo e settaggio della correta width a CONTROLLER-wrapper-nowrap
 		////////////  (come somma delle width e dei margin dei suoi figli)
 		var controller_content_with_ammount = 0;
-		$(".CONTROLLER-wrapper-nowrap > div").each(function(){
-			controller_content_with_ammount += parseInt($(this).css("width"))+28
+		$el.find(".CONTROLLER-wrapper-nowrap > div").each(function(){
+			controller_content_with_ammount += parseInt($(this).width())+28
 		});
 		
-		$(".CONTROLLER-wrapper-nowrap").css("width",controller_content_with_ammount)
+		$el.find(".CONTROLLER-wrapper-nowrap").css("width",controller_content_with_ammount)
+		
 		
 		// setto i controller laterali
 		var content_width = controller_content_with_ammount;
-		var wrapper_width = parseInt( $(".MAINAREA-controller").css("width") );
-		var wrapper_height = parseInt( $(".MAINAREA-controller").css("height") );
+		var wrapper_width = parseInt( $el.width() );
+		var wrapper_height = parseInt( $el.height() );
 		var overflow = content_width - wrapper_width
 		
 		
@@ -76,20 +86,18 @@ $(document).ready(function(){
 			return false;
 		
 		else{
-			$(".CONTROLLER-arrow-right").show();
-			$(".CONTROLLER-arrow-right,.CONTROLLER-arrow-left").css("height",wrapper_height-20);
-			$(".CONTROLLER-arrow-right > div , .CONTROLLER-arrow-left > div").css( "margin-top" , ((wrapper_height/2)-24) )
+			$el.find(".CONTROLLER-arrow-right").show();
+			$el.find(".CONTROLLER-arrow-right,.CONTROLLER-arrow-left").css("height",wrapper_height - scrollbar_height);
+			$el.find(".CONTROLLER-arrow-right > div , .CONTROLLER-arrow-left > div").css( "margin-top" , ((wrapper_height/2) - scrollbar_height) )
 			
 			
 			
 			var interval_right;	
-			$(".CONTROLLER-arrow-right").hover(
-					
-												
+			$el.find(".CONTROLLER-arrow-right").hover(							
 					function(){
 						interval_right = setInterval(function(){
-							scrollContent("ahead" , sensibility)
-						});
+							scrollContent("ahead" , sensibility , $el)
+						},interval);
 					},
 					function(){
 						clearInterval(interval_right);
@@ -97,11 +105,11 @@ $(document).ready(function(){
 			 );
 			
 			var interval_left;	
-			$(".CONTROLLER-arrow-left").hover(
+			$el.find(".CONTROLLER-arrow-left").hover(
 					function(){
 						interval_left = setInterval(function(){
-							scrollContent("backwards" , sensibility)
-						});
+							scrollContent("backwards" , sensibility , $el)
+						},interval);
 					},
 					function(){
 						clearInterval(interval_left);
@@ -109,34 +117,31 @@ $(document).ready(function(){
 			 );
 			
 			
-			
 			// gestione accensione/spengimento delle frecce
-			$(".CONTROLLER-wrapper").scroll(function() {
-				var scroll_x_left_offset =  Math.abs( $(".CONTROLLER-wrapper").position().left - $(".CONTROLLER-wrapper-nowrap").position().left );
-				var scroll_x_right_offset = Math.abs( $(".CONTROLLER-wrapper").position().left - $(".CONTROLLER-wrapper-nowrap").position().left - overflow );
+			$el.find(".CONTROLLER-wrapper").scroll(function() {
+				var scroll_x_left_offset =  Math.abs( $el.find(".CONTROLLER-wrapper").position().left - $el.find(".CONTROLLER-wrapper-nowrap").position().left );
+				var scroll_x_right_offset = Math.abs( $el.find(".CONTROLLER-wrapper").position().left - $el.find(".CONTROLLER-wrapper-nowrap").position().left - overflow );
 				
-				(scroll_x_left_offset <= hidding_arrows_offset) ? $(".CONTROLLER-arrow-left").fadeOut("slow") : $(".CONTROLLER-arrow-left").fadeIn();
-				(scroll_x_right_offset <= hidding_arrows_offset) ? $(".CONTROLLER-arrow-right").fadeOut("slow") : $(".CONTROLLER-arrow-right").fadeIn();
-				
+				(scroll_x_left_offset <= hidding_arrows_offset) ? $el.find(".CONTROLLER-arrow-left").fadeOut("slow") : $el.find(".CONTROLLER-arrow-left").fadeIn();
+				(scroll_x_right_offset <= hidding_arrows_offset) ? $el.find(".CONTROLLER-arrow-right").fadeOut("slow") : $el.find(".CONTROLLER-arrow-right").fadeIn();
 			});
 					  
-			
 		}
 
 	}
 	
-	function scrollContent(direction , sensibility){
+	function scrollContent(direction , sensibility , $el){
 		var speed = 5;
-		var arrow_width = $(".CONTROLLER-arrow-right").width();
+		var arrow_width = $el.find(".CONTROLLER-arrow-right").width();
 		
 		//alert(arrow_width)
 		if(direction == "backwards"){
-			speed = ( parseInt(( arrow_width - (mouseX - $(".CONTROLLER-wrapper").position().left) )/ sensibility) ) * -1;
+			speed = ( parseInt(( arrow_width - (mouseX - $el.find(".CONTROLLER-wrapper").position().left) )/ sensibility) ) * -1;
 		}
 		else{
-			speed = ((mouseX - $(".CONTROLLER-arrow-right").offset().left) / sensibility) //$(".CONTROLLER-wrapper").position().left 
+			speed = ((mouseX - $el.find(".CONTROLLER-arrow-right").offset().left) / sensibility) //$(".CONTROLLER-wrapper").position().left 
 		}
-		$(".CONTROLLER-wrapper").scrollTo( {top:'-=0px', left:'+='+speed});
+		$el.find(".CONTROLLER-wrapper").scrollTo( {top:'-=0px', left:'+='+speed});
 	}
 	
 	function calculateSlider(){
